3dfaae7a7bd86d0cbfc6772cd7d468ce482f4369
Bon Aguilan <aguilanbon@gmail.com>
2023-07-09 21:36:27 +0800

    added needed modeules for server, added port listener

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('node ts');
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compression from 'compression';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors({
+    credentials: true
+}))
+
+app.use(compression())
+app.use(cookieParser())
+app.use(bodyParser.json())
+
+
+app.listen(8080, () => console.log('server is running on: http://localhost:8080')
+);
