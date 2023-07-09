d0833d25f1e2dd7db92245f167d6726423dfa12a
Bon Aguilan <aguilanbon@gmail.com>
2023-07-09 22:05:23 +0800

    sample create

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,44 @@
-import express from 'express';
-import http from 'http';
-import bodyParser from 'body-parser'
-import cookieParser from 'cookie-parser'
-import compression from 'compression';
-import cors from 'cors';
-import 'dotenv/config'
-import mongoose from 'mongoose'
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+import { UserModel } from "./db/users";
 
 const app = express();
 
-app.use(cors({
-    credentials: true
-}))
-
-app.use(compression())
-app.use(cookieParser())
-app.use(bodyParser.json())
-
-
-app.listen(8080, () => console.log(`server is running on: ${process.env.SERVER_HOST}`)
+app.use(
+  cors({
+    credentials: true,
+  })
 );
 
-mongoose.connect(process.env.MONGO_COMPASS_URL).then(() => console.log('connected to db')
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+app.listen(8080, () =>
+  console.log(`server is running on: ${process.env.SERVER_HOST}`)
 );
-mongoose.connection.on('error', (error: Error) => console.log('error')
-);
+
+mongoose
+  .connect(process.env.MONGO_COMPASS_URL)
+  .then(() => console.log("connected to db"));
+mongoose.connection.on("error", (error: Error) => console.log("error"));
+
+app.get("/create", async (req, res) => {
+  const newUser = new UserModel({
+    username: "bonpuge",
+    email: "aguilanbon@gmail.com",
+    authentication: {
+      password: "123",
+      salt: "abc",
+      sessionToken: "abc123",
+    },
+  });
+
+  await newUser.save();
+});
