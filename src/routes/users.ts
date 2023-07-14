e1764fd61f2ce209aa053d2dcbb72d6efe10b50f
Bon Aguilan <aguilanbon@gmail.com>
2023-07-14 17:35:11 +0800

    updated signtout method

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -12,6 +12,6 @@ const auth_router = express.Router();
 auth_router.post("/register", registerUser);
 auth_router.post("/signin", signInUser);
 auth_router.get("/profile", isLoggedIn, getUserProfile);
-auth_router.post("/signout", isLoggedIn, signOutUser);
+auth_router.get("/signout", signOutUser);
 
 export default auth_router;
