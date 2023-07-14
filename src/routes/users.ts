538ce618753b61e321270c0d1749248d370eb168
Bon Aguilan <aguilanbon@gmail.com>
2023-07-14 16:56:28 +0800

    signout api

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,5 +1,10 @@
 import { isLoggedIn } from "../middleware/auth.middleware";
-import { getUserProfile, registerUser, signInUser } from "../controller/users";
+import {
+  getUserProfile,
+  registerUser,
+  signInUser,
+  signOutUser,
+} from "../controller/users";
 import express from "express";
 
 const auth_router = express.Router();
@@ -7,5 +12,6 @@ const auth_router = express.Router();
 auth_router.post("/register", registerUser);
 auth_router.post("/signin", signInUser);
 auth_router.get("/profile", isLoggedIn, getUserProfile);
+auth_router.get("/signout", isLoggedIn, signOutUser);
 
 export default auth_router;
