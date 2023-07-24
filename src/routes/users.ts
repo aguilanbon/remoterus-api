d0d50844bbe944ecd6e2c832dae1da9a5c58fd5a
Bon Aguilan <aguilanbon@gmail.com>
2023-07-24 11:48:14 +0800

    endpoint for refresh token

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,6 +1,7 @@
 import { isLoggedIn } from "../middleware/auth.middleware";
 import {
   getUserProfile,
+  refreshToken,
   registerUser,
   signInUser,
   signOutUser,
@@ -13,5 +14,6 @@ auth_router.post("/register", registerUser);
 auth_router.post("/signin", signInUser);
 auth_router.get("/profile", isLoggedIn, getUserProfile);
 auth_router.post("/signout", isLoggedIn, signOutUser);
+auth_router.get("/refresh", isLoggedIn, refreshToken);
 
 export default auth_router;
