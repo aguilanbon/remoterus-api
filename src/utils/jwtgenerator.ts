d52a9cdad5c58b8852c70ec3847073334bedce2b
Bon Aguilan <aguilanbon@gmail.com>
2023-07-24 10:41:25 +0800

    added methods to generate accesstoken and refreshtoken

--- src/utils/jwtgenerator.ts
+++ src/utils/jwtgenerator.ts
@@ -13,3 +13,16 @@ export const generateToken = (res: express.Response, userId: String) => {
     maxAge: 86400,
   });
 };
+
+export const generateAccessToken = (userId: String) => {
+  const accessToken = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "10s",
+  });
+  return accessToken;
+};
+export const generateRefreshToken = (userId: String) => {
+  const refreshToken = jwt.sign({ userId }, process.env.JWT_REFRESH_SECRET, {
+    expiresIn: "1d",
+  });
+  return refreshToken;
+};
